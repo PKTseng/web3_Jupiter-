8aa99d26b055b688bbc6c7186b178e9ba29dbb50
PKTseng <jk0910948659@gmail.com>
2025-04-17 01:16:41 +0800

    test slack

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,11 @@
-<template>test</template>
+<template>
+  {{ count }}
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(100)
+</script>
 
 <style lang="scss" scoped></style>
